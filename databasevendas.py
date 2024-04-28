94c2a358902180b788fc1d4d09f70c4684e95a79
Talvez Seja o Felipe <84604925+felipe0c4@users.noreply.github.com>
2024-04-28 01:19:21 -0300

    Delete databasevendas.py

--- databasevendas.py
+++
@@ -1,6 +0,0 @@
-vendasDB = [
-    {"item": "Camisa", "preço": 129.99, "quantidade": 1},
-    {"item": "Sapato", "preço": 200.00, "quantidade": 4},
-    {"item": "Meia", "preço": 20.50, "quantidade": 2},
-    {"item": "Bermuda", "preço": 53.60, "quantidade": 7},
-]
